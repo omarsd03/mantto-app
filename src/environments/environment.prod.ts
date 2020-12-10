1378d8b59048424c9701afb411a7833d2bd43677
Omar Salgado <Omar.Salgado@saint-gobain.com>
2020-12-09 19:32:28 -0600

    Cambio de Puerto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // TODO: Cambiar las URL para produccion
 export const environment = {
   production: true,
-  base_url: 'http://10.201.14.7:3000/mantto',
+  base_url: 'http://10.201.14.7:8080/mantto',
   abaddon: 'https://uat.abaddon.sgsm.saint-gobain.net/server/mantto'
 };
